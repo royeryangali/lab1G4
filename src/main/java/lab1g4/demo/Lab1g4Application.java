51a79fc7c354b03f456381fb5140407e860baab9
Royer <r.yangali@pucp.pe>
2020-09-15 08:08:04 -0500

    hola

---
+++ src/main/java/lab1g4/demo/Lab1g4Application.java
@@ -0,0 +1,13 @@
+package lab1g4.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1g4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1g4Application.class, args);
+    }
+
+}
---
+++ src/test/java/lab1g4/demo/Lab1g4ApplicationTests.java
@@ -0,0 +1,13 @@
+package lab1g4.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1g4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
